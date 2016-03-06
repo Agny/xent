c027354df63681b4057f3b47655114be7b0920b0
Agny <zyphon6@gmail.com>
2016-03-06 16:46:37 +0300

    resource state

--- core/src/main/scala/ru/agny/xent/Resource.scala
+++ core/src/main/scala/ru/agny/xent/Resource.scala
@@ -2,12 +2,17 @@ package ru.agny.xent
 
 sealed trait Resource[T <: Placed] {
   val name: String
-}
-case class Extractable(name: String) extends Resource[Global]
-case class Producible(name: String) extends Resource[Local]
 
-object Resource {
-  def apply(): Resource = {
-    ???
+  def produce(amount: Int): (Resource, ResourceUnit) // redo with functional state
+}
+case class Extractable(name: String, volume: Int) extends Resource[Global] {
+  override def produce(amount: Int): (Resource, ResourceUnit) = {
+    if (amount > volume) (this, ResourceUnit(this, 0))
+    else (Extractable(this.name, volume - amount), ResourceUnit(this, amount))
   }
 }
+case class Producible(name: String) extends Resource[Local] {
+  override def produce(amount: Int): (Resource, ResourceUnit) = (this, ResourceUnit(this, amount))
+}
+
+case class ResourceUnit(res: Resource, amount: Int)
