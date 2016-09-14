ea70975cba44f9bdbdecb20d0dc759d1182be3bb
Agny <zyphon6@gmail.com>
2016-09-14 14:53:32 +0300

    fix deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,9 @@ object Dependencies {
   val json4sJackson = "org.json4s" % "json4s-jackson_2.11" % "3.4.0" exclude("org.scala-lang.modules", "scala-xml_2.11")
 
   val scalactic = "org.scalactic" %% "scalactic" % "3.0.0"
-  val scalacticTest = "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  val scalacticTest = "org.scalatest" %% "scalatest" % "3.0.0" % "test" exclude("org.scala-lang.modules", "scala-xml_2.11")
   val netty = "io.netty" % "netty-all" % "4.1.4.Final"
 
-  val coreDeps = Seq(json4sJackson)
+  val coreDeps = Seq(json4sJackson, scalactic, scalacticTest)
   val webDeps = Seq(netty)
 }
