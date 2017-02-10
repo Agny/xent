1000cc11f4d12324bc72c3728a84625a1705c7b7
Agny <zyphon6@gmail.com>
2017-02-10 15:56:14 +0300

    troop vs troop

--- core/src/main/scala/ru/agny/xent/battle/unit/Troop.scala
+++ core/src/main/scala/ru/agny/xent/battle/unit/Troop.scala
@@ -3,7 +3,13 @@ package ru.agny.xent.battle.unit
 case class Troop(units: Seq[Unit]) {
 
   def attack(other: Troop): (Troop, Troop) = {
-    units.foldLeft(Seq.empty[Unit])((a,b) =>  a:+ b.attack(other)._1 )
+    val (u, t) = units.foldLeft((Seq.empty[Unit], other))(handleBattle)
+    (Troop(u), t)
+  }
+
+  private def handleBattle(state: (Seq[Unit], Troop), attacker: Unit): (Seq[Unit], Troop) = {
+    val (unitState, newTroopState) = attacker.attack(state._2)
+    (state._1 :+ unitState, newTroopState)
   }
 
 }
