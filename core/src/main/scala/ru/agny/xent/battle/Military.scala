b9ac9922558145d34122eee43b3ef8239ad0e42b
Agny <zyphon6@gmail.com>
2017-05-18 22:00:39 +0300

    advanced check to battle commencing

--- core/src/main/scala/ru/agny/xent/battle/Military.scala
+++ core/src/main/scala/ru/agny/xent/battle/Military.scala
@@ -6,49 +6,77 @@ import ru.agny.xent.core.utils.SubTyper
 
 case class Military(troops: Vector[(Troop, Occupation)]) {
 
-  import ru.agny.xent.battle.OccupationSubTyper.implicits._
+  import ru.agny.xent.battle.Military._
 
   def tick: (Military, Vector[Troop]) = {
-    val troopsPositions = troops.foldLeft(Map.empty[Coordinate, Vector[(Troop, Occupation)]])(
-      (positioned, t) => addPos(positioned, t._2.pos(t._1.moveSpeed, System.currentTimeMillis()), t)
-    )
-    val (alive, fallen) = troopsPositions.foldLeft(Vector.empty[(Troop, Occupation)], Vector.empty[Troop]) {
-      (a, b) => val (inProcess, out) = findBattle(b._1, b._2)
-        (inProcess ++ a._1, out ++ a._2)
+    val empty = Map.empty[Coordinate, Vector[TO]].withDefaultValue(Vector.empty)
+    val troopsPositions = troops.foldLeft(empty) { (positioned, t) =>
+      val pos = t._2.pos(t._1.moveSpeed, System.currentTimeMillis())
+      addPos(positioned, pos, t)
     }
-    (Military(alive), fallen)
+    val (alive, toErase) = troopsPositions.foldLeft(Vector.empty[TO], Vector.empty[Troop]) { (a, b) =>
+      val (inBattle, out) = findBattle(b._1, b._2)
+      val (moving, fallenArrived) = arrive(out)
+      (moving ++ inBattle ++ a._1, fallenArrived ++ a._2)
+    }
+    (Military(alive), toErase)
   }
+}
 
-  private def addPos(poss: Map[Coordinate, Vector[(Troop, Occupation)]], pos: Coordinate, t: (Troop, Occupation)) =
-    if (poss.contains(pos)) {
-      val other = poss(pos)
-      poss.updated(pos, t +: other)
-    } else poss.updated(pos, Vector(t))
+object Military {
 
-  private def findBattle(pos: Coordinate, inArea: Vector[(Troop, Occupation)]): (Vector[(Troop, Occupation)], Vector[Troop]) = inArea match {
+  import ru.agny.xent.battle.OccupationSubTyper.implicits._
+
+  private type TO = (Troop, Occupation)
+  private type TB = (Troop, Battle)
+
+  val empty = Military(Vector.empty)
+
+  private def addPos(ct: Map[Coordinate, Vector[TO]], pos: Coordinate, t: TO) = ct.updated(pos, t +: ct(pos))
+
+  //TODO fallen troop should ignore fights
+  private def findBattle(pos: Coordinate, inArea: Vector[TO]): (Iterable[TB], Iterable[TO]) = inArea match {
     case multiple@h +: t if t.nonEmpty =>
       val (inBattle, queueing) = partition[Battle](multiple)
-      val (battle, fallen) = commenceBattle(pos, inBattle, queueing).tick
-      (multiple.map(x => (x._1, battle)), fallen)
-    case single => (single, Vector.empty)
+      val (battle, moving) = commenceBattle(pos, inBattle, queueing)
+      val tb = battle match {
+        case Some(b) => b.troops.map(x => (x._1, b)) ++ b.queue.map(x => (x._1, b))
+        case _ => Vector.empty
+      }
+      (tb, moving)
+    case x => (Vector.empty, x)
   }
 
-  private def partition[By <: Occupation](troops: Vector[(Troop, Occupation)])(implicit ev: SubTyper[Occupation, By]) = {
+  private def partition[By <: Occupation](troops: Vector[TO])(implicit ev: SubTyper[Occupation, By]) = {
     val l = Vector.empty[(Troop, By)]
-    val r = Vector.empty[(Troop, Occupation)]
+    val r = Vector.empty[TO]
     troops.foldLeft(l, r)((lr, x) => (x._1, ev.asSub(x._2)) match {
       case (a, Some(o)) => ((a, o) +: lr._1, lr._2)
       case _ => (lr._1, x +: lr._2)
     })
   }
 
-  private def commenceBattle(pos: Coordinate, inBattle: Vector[(Troop, Battle)], queueing: Vector[(Troop, Occupation)]): Battle = inBattle match {
-    case (_, b) +: _ => b.addTroops(queueing.unzip._1.toVector)
-    case _ => Battle(pos, queueing.unzip._1.toVector)
+  private def commenceBattle(pos: Coordinate, inBattle: Vector[TB], queueing: Vector[TO]): (Option[Battle], Vector[TO]) = inBattle match {
+    case (_, b) +: _ =>
+      val (inProcess, leaving) = b.tick
+      inProcess match {
+        case Some(battle) => (Some(battle.addTroops(queueing.toMap)), leaving)
+        case None if isSameUserArmies(queueing.unzip._1) => (None, leaving ++ queueing)
+        case _ => (Some(Battle(pos, queueing.toMap)), leaving)
+      }
+    case _ if isSameUserArmies(queueing.unzip._1) => (None, queueing)
+    case _ => Battle(pos, queueing.toMap).tick
   }
 
-}
+  //TODO send troop back to city upon arriving
+  private def arrive(troops: Iterable[TO]): (Vector[TO], Vector[Troop]) = troops.foldLeft(Vector.empty[TO], Vector.empty[Troop]) { (res, x) =>
+    val now = System.currentTimeMillis()
+    x match {
+      case (fallen, m: Movement) if fallen.activeUnits.isEmpty && m.pos(10, now) == m.to => (res._1, fallen +: res._2)
+      case (alive, m: Movement) if m.pos(alive.moveSpeed, now) == m.to => ((alive, new Waiting(m.to, now)) +: res._1, res._2)
+      case _ => (x +: res._1, res._2)
+    }
+  }
 
-object Military {
-  val empty = Military(Vector.empty)
+  private def isSameUserArmies(t: Vector[Troop]) = t.map(x => x.user).distinct.size < 2
 }
