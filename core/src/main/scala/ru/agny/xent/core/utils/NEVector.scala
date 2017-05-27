486a498479b617836a693654292c60c48d181fb1
Agny <zyphon6@gmail.com>
2017-05-27 19:55:28 +0300

    redo nonempty Seq

--- core/src/main/scala/ru/agny/xent/core/utils/NEVector.scala
+++
@@ -1,7 +0,0 @@
-package ru.agny.xent.core.utils
-
-case class NEVector[A](override val head: A, override val tail: Vector[A]) extends Traversable[A] {
-  private val iterator = (head +: tail).iterator
-
-  override def foreach[U](f: (A) => U): Unit = iterator.foreach(f)
-}
