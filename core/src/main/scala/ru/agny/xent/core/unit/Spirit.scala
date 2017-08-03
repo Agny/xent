e99acef0844463a249267ce1474325fad88b5e22
Agny <zyphon6@gmail.com>
2017-08-04 01:33:30 +0300

    spirit to life power

--- core/src/main/scala/ru/agny/xent/core/unit/Spirit.scala
+++ core/src/main/scala/ru/agny/xent/core/unit/Spirit.scala
@@ -2,4 +2,6 @@ package ru.agny.xent.core.unit
 
 case class Spirit(points: Int, regen: Int, capacity: Int) {
   def change(x: Int) = Spirit(points + x, regen, capacity)
+
+  def toLifePower: Int = capacity
 }
