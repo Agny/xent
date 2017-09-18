9e4d14d58b3b98b9c67d58401f213f88385438f8
Agny <zyphon6@gmail.com>
2017-09-18 17:57:04 +0300

    replace anonymous classes by SAMs

--- core/src/main/scala/ru/agny/xent/core/inventory/ItemLike.scala
+++ core/src/main/scala/ru/agny/xent/core/inventory/ItemLike.scala
@@ -3,15 +3,11 @@ package ru.agny.xent.core.inventory
 trait ItemLike[To <: Item, From <: Item] {
   def cast(v: From): (Option[From], Option[To])
 }
-//TODO SAM
+
 object ItemLike {
   object implicits {
-    implicit def isNotSub[To <: Item, From <: Item]: ItemLike[To, From] = new ItemLike[To, From] {
-      override def cast(v: From): (Option[From], Option[To]) = (Some(v), None)
-    }
+    implicit def isNotSub[To <: Item, From <: Item]: ItemLike[To, From] = (v: From) => (Some(v), None)
 
-    implicit def isSub[To <: Item, From <: To]: ItemLike[To, From] = new ItemLike[To, From] {
-      override def cast(v: From): (Option[From], Option[To]) = (Some(v), Some(v))
-    }
+    implicit def isSub[To <: Item, From <: To]: ItemLike[To, From] = (v: From) => (Some(v), Some(v))
   }
 }
