53fd9d7b98f091c009b275355bc914c2f5f34e39
Agny <zyphon6@gmail.com>
2017-10-29 18:08:00 +0300

    make only ItemStack is lootable

--- core/src/main/scala/ru/agny/xent/battle/Loot.scala
+++ core/src/main/scala/ru/agny/xent/battle/Loot.scala
@@ -1,14 +1,14 @@
 package ru.agny.xent.battle
 
-import ru.agny.xent.core.inventory.Item
+import ru.agny.xent.core.inventory.ItemStack
 
 trait Loot {
-  def get: Vector[Item]
+  def get: Vector[ItemStack]
 }
 
 object Loot {
-  def apply(items: Vector[Item]): Loot = InnerLoot(items)
-  private case class InnerLoot(items: Vector[Item]) extends Loot {
+  def apply(items: Vector[ItemStack]): Loot = InnerLoot(items)
+  private case class InnerLoot(items: Vector[ItemStack]) extends Loot {
     override def get = items
   }
 }
