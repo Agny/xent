3fe65fb999bd16f85ee8c0b1c6830700157ab84e
Agny <zyphon6@gmail.com>
2017-11-01 15:28:53 +0300

    small fix

--- core/src/main/scala/ru/agny/xent/core/inventory/Cost.scala
+++ core/src/main/scala/ru/agny/xent/core/inventory/Cost.scala
@@ -2,7 +2,7 @@ package ru.agny.xent.core.inventory
 
 case class Cost(v: Vector[Item]) {
   def price(amount: Int) = Cost(v.foldLeft(Vector.empty[Item]) {
-    case (acc, x: ItemStack, ) => ItemStack(x.stackValue * amount, x.id) +: acc
+    case (acc, x: ItemStack) => ItemStack(x.stackValue * amount, x.id) +: acc
     case (acc, item) => (0 until amount map (_ => item)).toVector ++ acc
   })
 }
