80c20033ae7a72a91161f91e5f6509b96bac6fa5
Agny <zyphon6@gmail.com>
2017-11-01 14:54:01 +0300

    simplify handler, actions now responsible for responding

--- core/src/main/scala/ru/agny/xent/core/LayerRuntime.scala
+++ core/src/main/scala/ru/agny/xent/core/LayerRuntime.scala
@@ -1,105 +1,60 @@
 package ru.agny.xent.core
 
-import ru.agny.xent.action._
-import ru.agny.xent.messages._
-import ru.agny.xent.messages.production._
-import ru.agny.xent.messages.unit.{CreateSoulMessage, CreateTroopMessage}
+import ru.agny.xent.action.{LayerAction, UserAction}
+import ru.agny.xent.core.utils.ErrorCode
+import ru.agny.xent.messages.{ActiveMessage, LayerUpMessage, Message, MessageQueue}
 
-class LayerRuntime(queue: MessageQueue[Responder[_]]) {
+class LayerRuntime(queue: MessageQueue[Message]) {
   private var lastState: Vector[Layer] = Vector.empty
 
   private def run(initialState: Vector[Layer]): Vector[Layer] = {
     lastState = initialState
-
-    new Thread(new Runnable {
-      override def run(): Unit = {
-        while (true) {
-          lastState = rec(lastState, queue.take())
-          Thread.sleep(1000)
-        }
+    new Thread(() => {
+      while (true) {
+        lastState = handle(lastState, queue.take())
+        Thread.sleep(1000)
       }
     }).start()
 
     lastState
   }
 
-  private def rec(startState: Vector[Layer], messages: Vector[Responder[_]]): Vector[Layer] = {
-    messages.foldLeft(startState)((layers, m) => m match {
-      //TODO need some kind of abstraction for this handling
-      case x: NewUserMessage =>
-        layers.find(l => l.id == x.layer) match {
-          case Some(layer) => layer.tick(NewUser(x.user, x.name)) match {
-            case Left(v) => x.respond(v); layers
-            case Right(v) => x.respond(ResponseOk); v +: layers.diff(Vector(layer))
+  private def handle(startState: Vector[Layer], messages: Vector[Message]): Vector[Layer] = {
+    messages.foldLeft(startState)((layers, m) => runAction(m, layers))
+  }
+
+  private def runAction(msg: Message, layers: Vector[Layer]): Vector[Layer] = {
+    msg match {
+      case specialCase: LayerUpMessage =>
+        val (active, idle) = layers.partition(l => l.id == specialCase.layer || l.id == specialCase.layerTo)
+        val from = active.find(l => l.id == specialCase.layer).get
+        val to = active.find(l => l.id == specialCase.layerTo).get
+        val (updatedFrom, updatedTo) = specialCase.action.run(from, to)
+        Vector(updatedFrom, updatedTo) ++ idle
+      case a: ActiveMessage => a.action match {
+        case x: UserAction =>
+          layers.find(l => l.id == msg.layer) match {
+            case Some(layer) =>
+              val l = layer.tick(x, msg.user)
+              if (l == layer) layers else l +: layers.diff(Vector(layer))
+            case None => a.failed(ErrorCode.LAYER_NOT_FOUND); layers
           }
-          case None => x.respond(PlainResponse(s"Layer[${x.layer}] isn't found")); layers
-        }
-      case x: LayerUpMessage =>
-        val (active, idle) = layers.partition(l => l.id == x.layer || l.id == x.layerTo)
-        val from = active.find(l => l.id == x.layer).get
-        val to = active.find(l => l.id == x.layerTo).get
-        LayerChange(x.user).run(from, to) match {
-          case Left(v) => x.respond(v); layers
-          case Right((layerFrom, layerTo)) => x.respond(ResponseOk); Vector(layerFrom, layerTo) ++ idle
-        }
-      case x: ResourceClaimMessage =>
-        layers.find(l => l.id == x.layer) match {
-          case Some(layer) => layer.tick(ResourceClaim(x.facility, x.user, x.cell)) match {
-            case Left(v) => x.respond(v); layers
-            case Right(v) => x.respond(ResponseOk); v +: layers.diff(Vector(layer))
+        case x: LayerAction =>
+          layers.find(l => l.id == msg.layer) match {
+            case Some(layer) =>
+              val l = layer.tick(x)
+              if (l == layer) layers else l +: layers.diff(Vector(layer))
+            case None => a.failed(ErrorCode.LAYER_NOT_FOUND); layers
           }
-          case None => x.respond(PlainResponse(s"Layer[${x.layer}] isn't found")); layers
-        }
-      case x: BuildingConstructionMessage =>
-        layers.find(l => l.id == x.layer) match {
-          case Some(layer) => layer.tick(PlaceBuilding(x.building, layer, x.cell), x.user) match {
-            case Left(v) => x.respond(v); layers
-            case Right(v) => x.respond(ResponseOk); v +: layers.diff(Vector(layer))
-          }
-          case None => x.respond(PlainResponse(s"Layer[${x.layer}] isn't found")); layers
-        }
-      case x: AddProductionMessage =>
-        layers.find(l => l.id == x.layer) match {
-          case Some(layer) => layer.tick(AddProduction(x.facility, x.res), x.user) match {
-            case Left(v) => x.respond(v); layers
-            case Right(v) => x.respond(ResponseOk); v +: layers.diff(Vector(layer))
-          }
-          case None => x.respond(PlainResponse(s"Layer[${x.layer}] isn't found")); layers
-        }
-      case x: CreateSoulMessage =>
-        layers.find(l => l.id == x.layer) match {
-          case Some(layer) => layer.tick(CreateSoul(x.baseSpirit, x.stats.map(_.lift)), x.user) match {
-            case Left(v) => x.respond(v); layers
-            case Right(v) => x.respond(ResponseOk); v +: layers.diff(Vector(layer))
-          }
-          case None => x.respond(PlainResponse(s"Layer[${x.layer}] isn't found")); layers
-        }
-      case x: CreateTroopMessage =>
-        layers.find(l => l.id == x.layer) match {
-          case Some(layer) => layer.tick(CreateTroop(x.user, x.souls)) match {
-            case Left(v) => x.respond(v); layers
-            case Right(v) => x.respond(ResponseOk); v +: layers.diff(Vector(layer))
-          }
-          case None => x.respond(PlainResponse(s"Layer[${x.layer}] isn't found")); layers
-        }
-      //          //TODO this does pretty nothing
-      //          case x: EmptyMessage =>
-      //            layers.find(l => l.id == x.layer) match {
-      //              case Some(layer) => layer.tick(Idle(x.user), x.user) match {
-      //                case Left(v) => x.reply(v); layers
-      //                case Right(v) => x.reply(ResponseOk); v +: layers.diff(Vector(layer))
-      //              }
-      //              case None => x.reply(Response(s"Layer[${x.layer}] isn't found")); layers
-      //            }
-      case _ => layers
-    })
+      }
+    }
   }
 
   def get = lastState
 }
 
 object LayerRuntime {
-  def run(layers: Vector[Layer], queue: MessageQueue[Responder[_]]) = {
+  def run(layers: Vector[Layer], queue: MessageQueue[Message]) = {
     val runtime = new LayerRuntime(queue)
     runtime.run(layers)
     runtime
