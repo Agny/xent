9e96695134f90c651f4dcf6a4d5777931ed144a5
Agny <zyphon6@gmail.com>
2017-11-27 20:02:05 +0300

    add slick

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,6 +11,11 @@ object Dependencies {
   val circe = Vector("io.circe" %% "circe-core", "io.circe" %% "circe-generic", "io.circe" %% "circe-parser").map(_ % "0.8.0")
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.0" % Test
   val scalaRedis = "net.debasishg" %% "redisclient" % "3.4"
+  val slick = Seq(
+    "com.typesafe.slick" %% "slick" % "3.2.1",
+    "org.slf4j" % "slf4j-nop" % "1.6.4",
+    "com.typesafe.slick" %% "slick-hikaricp" % "3.2.1"
+  )
 
   val netty = "io.netty" % "netty-all" % "4.1.4.Final"
 
@@ -25,7 +30,7 @@ object Dependencies {
   )
 
   val macrosDeps = Vector(reflect, parserCombinator)
-  val coreDeps = Vector(scalatest, scalaRedis, akkaActor) ++ circe
+  val coreDeps = Vector(scalatest, scalaRedis, akkaActor) ++ circe ++ slick
   val webDeps = Vector(netty)
   val marketDeps = akkaStream ++ akkaHttp
 }
