bd144ece44f2ff0b82e242f9261f236fa3cfad26
Agny <zyphon6@gmail.com>
2017-12-06 21:50:22 +0300

    Price is comparable

--- market/src/main/scala/ru/agny/xent/trade/Price.scala
+++ market/src/main/scala/ru/agny/xent/trade/Price.scala
@@ -2,4 +2,6 @@ package ru.agny.xent.trade
 
 import ru.agny.xent.core.inventory.ItemStack
 
-case class Price(amount: ItemStack)
+case class Price(amount: ItemStack) extends Ordered[Price] {
+  override def compare(that: Price) = amount.stackValue - that.amount.stackValue
+}
