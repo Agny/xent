613d0e3679ea52d8133b233ef45499f4bb8541dc
Agny <zyphon6@gmail.com>
2018-01-03 16:49:34 +0300

    add ReservedItemRepository

---
+++ market/src/main/scala/ru/agny/xent/trade/persistence/slick/ReservedItemRepository.scala
@@ -0,0 +1,19 @@
+package ru.agny.xent.trade.persistence.slick
+
+import ru.agny.xent.core.utils.UserType.UserId
+import ru.agny.xent.persistence.slick.ConfigurableRepository
+import ru.agny.xent.trade.persistence.slick.ReservedItemEntity.ReservedFlat
+
+import scala.concurrent.Future
+import slick.jdbc.PostgresProfile.api._
+
+case class ReservedItemRepository(configPath: String) extends ConfigurableRepository {
+
+  private val reserved = ReservedItemEntity.table
+
+  def findByUser(user: UserId): Future[Seq[ReservedFlat]] = {
+    val query = reserved.filter(_.userId === user)
+    db.run(query.result)
+  }
+
+}
