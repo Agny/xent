e9dc8acaf85c42ee106742fa4f1c1a3dc5a3cdbe
Agny <zyphon6@gmail.com>
2018-01-11 22:02:54 +0300

    note OOM error

--- market/src/main/scala/ru/agny/xent/trade/utils/SynchronizedPool.scala
+++ market/src/main/scala/ru/agny/xent/trade/utils/SynchronizedPool.scala
@@ -1,9 +1,8 @@
 package ru.agny.xent.trade.utils
 
-import java.util
-import java.util.Collections
 import java.util.concurrent.{Callable, Executors}
 
+import scala.collection.concurrent.TrieMap
 import scala.concurrent.Future
 
 /**
@@ -11,13 +10,14 @@ import scala.concurrent.Future
   */
 class SynchronizedPool() {
   val threadPool = Executors.newCachedThreadPool()
-  val locks = Collections.synchronizedMap(new util.WeakHashMap[Any, AnyRef]())
+  val locks = TrieMap.empty[Any, AnyRef] //TODO NB:schedule cleanup / replace by simple lru cache ?
 
   def submit[T](v: Callable[T]): Future[T] = {
     v match {
       case t: SynchronizableTask[T] =>
         locks.putIfAbsent(t.syncField, new Object())
-        Future.successful(threadPool.submit(new WrapperTask[T](t, locks.get(t.syncField))).get())
+        val task = new WrapperTask[T](t, locks.get(t.syncField))
+        Future.successful(threadPool.submit(task).get())
     }
   }
 }
