b87104089482df9dfff3a690830524570c4d4f50
Agny <zyphon6@gmail.com>
2018-01-11 20:25:54 +0300

    guarding from concurrent access with synchronized pool

--- market/src/main/scala/ru/agny/xent/trade/Board.scala
+++ market/src/main/scala/ru/agny/xent/trade/Board.scala
@@ -1,105 +1,127 @@
 package ru.agny.xent.trade
 
-import akka.stream.scaladsl.Source
 import com.typesafe.scalalogging.LazyLogging
 import ru.agny.xent.core.Layer.LayerId
 import ru.agny.xent.core.utils.UserType.UserId
 import ru.agny.xent.messages.{PlainResponse, ResponseFailure, ResponseOk}
 import ru.agny.xent.trade.Lot.LotId
 import ru.agny.xent.trade.persistence.slick.LotRepository
+import ru.agny.xent.trade.utils.{SynchronizableTask, SynchronizedPool}
 
 import scala.concurrent.ExecutionContext.Implicits._
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
 case class Board(layer: LayerId, dbConfig: String) extends LazyLogging {
 
   import ru.agny.xent.trade.Board._
+  import scala.concurrent.duration._
 
   private val lotRepository = LotRepository(dbConfig)
 
-  def lots(start: Int = 0, end: Int = 50) = {
-    val r = lotRepository.load(start, end)
-    Source.fromPublisher(r)
-  }
+  def lots(start: Int = 0, end: Int = 50) = lotRepository.load(start, end)
 
-  def offer[T <: WSRequest : WSAdapter](msg: BoardMessage): Future[PlainResponse] = {
+  def offer[T <: WSRequest : WSAdapter](msg: BoardMessage)(implicit pool: SynchronizedPool): Future[PlainResponse] = {
     logger.debug("got message {}", msg)
     msg match {
       case Add(lot) => addLot(lot)
-      case Buy(lot, bid) => buy(lot, bid)
-      case PlaceBid(lot, bid) => placeBid(lot, bid)
-      case Sell(lot, amount, user) => sell(lot, amount, user)
+      case Buy(lot, bid) => pool.submit(buy(lot, bid))
+      case PlaceBid(lot, bid) => pool.submit(placeBid(lot, bid))
+      case Sell(lot, amount, user) => pool.submit(sell(lot, amount, user))
     }
   }
 
   private def addLot[T <: WSRequest : WSAdapter](lot: PlaceLot): Future[PlainResponse] = {
     lotRepository.create(lot).flatMap(lotId =>
-      verifyPlacement(lot.user, lot.item).map {
+      verifyPlacement(lot.user, lot.item).flatMap {
         case ResponseFailure =>
-          lotRepository.delete(lotId).recover {
-            case t: Throwable => logger.error("unverified record cleanup failed {}", t)
+          lotRepository.delete(lotId).map(_ => ResponseFailure).recover {
+            case t: Throwable =>
+              logger.error("unverified record cleanup failed {}", t)
+              ResponseFailure
           }
-          ResponseFailure
-        case x => x
+        case x => Future.successful(x)
       }
     )
   }
 
-  /**
-    * Buy operation is considered complete if following conditions are met:
-    * 1) lot to buy exists in database;
-    * 2) buyer has sufficient funds to cover lot price;
-    * 3) lot successfully deleted.
-    *
-    */
-  private def buy[T <: WSRequest : WSAdapter](lotId: LotId, buyer: UserId): Future[PlainResponse] = {
-    val prepareToSpend = for {
-      lot <- lotRepository.buyPreparement(lotId, buyer)
-      isAllowed <- verifyPlacement(buyer, lot.item)
-    } yield (lot, isAllowed)
-
-    prepareToSpend.transformWith {
-      case Success((lot, isAllowed)) => buyComplete(lot, buyer, isAllowed)
-      case Failure(t) =>
-        logger.error("lot buyout failed", t.getMessage)
-        Future.failed(t)
-    }
-  }
-
-  private def placeBid[T <: WSRequest : WSAdapter](lot: LotId, bid: Bid): Future[PlainResponse] = {
-    val prepareToSpend = for {
-      lastBid <- lotRepository.biddingPreparement(lot, bid)
-      verified <- verifyPlacement(bid.owner, bid.price)
-    } yield (lastBid, verified)
-
-    prepareToSpend.transformWith {
-      case Success((prevBid, isAllowed)) => bidPlacingComplete(lot, bid, prevBid, isAllowed)
-      case Failure(t) =>
-        logger.error("lot bid failed", t.getMessage)
-        Future.failed(t)
-    }
-  }
-
-  private def sell[T <: WSRequest : WSAdapter](lotId: LotId, amount: Int, user: UserId): Future[PlainResponse] = {
-    lotRepository.read(lotId).flatMap {
-      case Some(lotRead: Dealer) if lotRead.tpe == Dealer.`type` =>
-        val soldPrice = (sold: Int) => ItemHolder(lotRead.buyout.id, sold * lotRead.buyout.amount)
-        val boughtItems = (sold: Int) => ItemHolder(lotRead.item.id, sold)
-        for {
-          sold <- lotRepository.sell(lotId, user, amount)
-          verified <- verifyPlacement(user, soldPrice(sold))
-          _ <- complete(verified,
-            sendItems(lotRead, (lotRead.user, boughtItems(sold)), (user, soldPrice(sold))),
-            lotRepository.revertSell(lotId, sold))
-        } yield verified
+  private def buy[T <: WSRequest : WSAdapter](lotId: LotId, buyer: UserId) = {
+    val result = lotRepository.read(lotId).flatMap {
       case None =>
         logger.info("lot {} is not found", lotId)
         Future.successful(ResponseFailure)
-      case _ =>
-        logger.warn("lot {} is not sellable", lotId)
+      case Some(lot) if lot.tpe == Dealer.`type` =>
+        logger.warn("incorrect lot type {} to buy", lot)
         Future.successful(ResponseFailure)
+      case Some(lot) if lot.user == buyer =>
+        logger.warn("user can't buy {} from himself", lot)
+        Future.successful(ResponseFailure)
+      case Some(lot) =>
+        verifyPlacement(buyer, lot.item).transformWith {
+          case Success(verified) => buyComplete(lot, buyer, verified)
+          case Failure(t) =>
+            logger.error("buyout failed", t.getMessage)
+            Future.failed(t)
+        }
     }
+    new SynchronizableTask(lotId, Await.result(result, 4 seconds))
+  }
+
+  private def placeBid[T <: WSRequest : WSAdapter](lotId: LotId, bid: Bid) = {
+    val result = lotRepository.read(lotId).flatMap {
+      case None =>
+        logger.info("lot {} is not found", lotId)
+        Future.successful(ResponseFailure)
+      case Some(lot) if lot.tpe != NonStrict.`type` =>
+        logger.warn("{} is not biddable", lot)
+        Future.successful(ResponseFailure)
+      case Some(lot) if lot.user == bid.owner =>
+        logger.warn("user can't buy {} from himself", lot)
+        Future.successful(ResponseFailure)
+      case Some(lot: NonStrict) =>
+        lot match {
+          case NonStrict(_, _, _, _, minPrice, _, _) if minPrice.amount > bid.price.amount =>
+            logger.warn("{} is less than minimal lot price {}", bid, lot)
+            Future.successful(ResponseFailure)
+          case NonStrict(_, _, _, _, _, _, Some(last)) if last.price.amount >= bid.price.amount =>
+            logger.warn("{} is less than last bid price {}", bid, lot.lastBid)
+            Future.successful(ResponseFailure)
+          case ok =>
+            for {
+              verified <- verifyPlacement(bid.owner, bid.price)
+              _ <- bidPlacingComplete(lot, bid, verified)
+            } yield verified
+        }
+    }
+    new SynchronizableTask(lotId, Await.result(result, 4 seconds))
+  }
+
+  private def sell[T <: WSRequest : WSAdapter](lotId: LotId, amount: Int, user: UserId) = {
+    val result = lotRepository.read(lotId).flatMap {
+      case None =>
+        logger.info("lot {} is not found", lotId)
+        Future.successful(ResponseFailure)
+      case Some(lot) if lot.tpe != Dealer.`type` =>
+        logger.warn("lot {} is not sellable", lot)
+        Future.successful(ResponseFailure)
+      case Some(lot) if lot.item.amount <= 0 =>
+        logger.warn("lot {} is complete", lot)
+        Future.successful(ResponseFailure)
+      case Some(lot) if lot.user == user =>
+        logger.warn("user can't sell {} to himself", lot)
+        Future.successful(ResponseFailure)
+      case Some(lot: Dealer) =>
+        val soldPrice = (sold: Int) => ItemHolder(lot.buyout.id, sold * lot.buyout.amount)
+        val boughtItems = (sold: Int) => ItemHolder(lot.item.id, sold)
+        for {
+          (remains, sold) <- lotRepository.sell(lotId, user, amount)
+          verified <- verifyPlacement(user, soldPrice(sold))
+          _ <- complete(verified == ResponseOk,
+            sellComplete(lot, remains, boughtItems(sold), user, soldPrice(sold)),
+            lotRepository.revertSell(lotId, sold))
+        } yield verified
+    }
+    new SynchronizableTask(lotId, Await.result(result, 4 seconds))
   }
 
   private def verifyPlacement[T <: WSRequest : WSAdapter](byUser: UserId, payment: ItemHolder): Future[PlainResponse] = {
@@ -114,10 +136,10 @@ case class Board(layer: LayerId, dbConfig: String) extends LazyLogging {
     }
   }
 
-  private def complete(verification: PlainResponse,
+  private def complete(p: => Boolean,
                        success: => Future[Boolean],
                        cancellation: => Future[Boolean]) = {
-    if (verification == ResponseFailure) cancellation else success
+    if (p) success else cancellation
   }
 
   private def sendItems[T <: WSRequest : WSAdapter](lot: Lot, itemOut: (UserId, ItemHolder), itemIn: (UserId, ItemHolder)): Future[Boolean] = {
@@ -156,14 +178,12 @@ case class Board(layer: LayerId, dbConfig: String) extends LazyLogging {
   private def buyComplete[T <: WSRequest : WSAdapter](lot: Lot, buyer: UserId, verification: PlainResponse) = {
     verification match {
       case ResponseOk =>
-        lotRepository.delete(lot.id).transform {
+        lotRepository.delete(lot.id).transformWith {
           case Success(true) =>
-            sendItems(lot, (lot.user, lot.item), (buyer, lot.buyout))
-            Success(ResponseOk)
+            sendItems(lot, (lot.user, lot.item), (buyer, lot.buyout)).map(_ => ResponseOk)
           case x@(Failure(_) | Success(false)) =>
             logger.error("bought lot deletion failure {}", x)
-            returnItemsToOwner(buyer, lot.buyout)
-            Success(ResponseFailure)
+            returnItemsToOwner(buyer, lot.buyout).map(_ => ResponseFailure)
         }
       case ResponseFailure =>
         logger.warn("buy operation is denied for buyer {} and lot {}", buyer, lot)
@@ -171,27 +191,33 @@ case class Board(layer: LayerId, dbConfig: String) extends LazyLogging {
     }
   }
 
-  private def bidPlacingComplete[T <: WSRequest : WSAdapter](lotId: LotId,
+  private def bidPlacingComplete[T <: WSRequest : WSAdapter](lot: NonStrict,
                                                              bid: Bid,
-                                                             lastBid: Option[Bid],
                                                              verification: PlainResponse): Future[PlainResponse] = {
     verification match {
       case ResponseOk =>
         val updateResult = for {
-          res <- lotRepository.insertBid(lotId, bid)
-          _ <- returnOldBidToOwner(lastBid)
+          res <- lotRepository.updateBid(lot.id, bid)
+          _ <- returnOldBidToOwner(lot.lastBid)
         } yield res
 
         updateResult.transformWith {
           case Success(true) => Future.successful(ResponseOk)
           case x@(Failure(_) | Success(false)) =>
-            logger.error("lot {} update by bid {} failure {}", lotId, bid, x)
+            logger.error("{} update by bid {} failure {}", lot, bid, x)
             returnItemsToOwner(bid.owner, bid.price).map(_ => ResponseFailure)
         }
-      case ResponseFailure =>
-        logger.warn("lot[{}] update is denied for bid {}", lotId, bid)
-        lotRepository.revertBid(lotId, lastBid).flatMap(_ => Future.successful(ResponseFailure))
+      case x =>
+        logger.warn("{} update is denied for bid {}", lot, bid)
+        Future.successful(x)
     }
+  }
+
+  private def sellComplete[T <: WSRequest : WSAdapter](lot: Lot, remains: Int, bought: ItemHolder, seller: UserId, sold: ItemHolder) = {
+    for {
+      _ <- complete(remains == 0, lotRepository.delete(lot.id), Future.successful(true))
+      x <- sendItems(lot, (lot.user, bought), (seller, sold))
+    } yield x
   }
 }
 
